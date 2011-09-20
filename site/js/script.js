51e284bb32cf2c795fcdcd8f67f8db542fddc7dc
Antoine Neveux <antoine.neveux@gmail.com>
2011-09-20 09:44:25 +0200

    Added //J home page in repository

---
+++ site/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	
+	$("body").corner("20px");
+	
+});
+
+var scrolling = function(){
+			    var speed     = 2000;
+			    jQuery('a[href^="#"]').bind('click',function(){
+			        var id = jQuery(this).attr('href');
+			        if(id == '#')
+			            goTo('body');
+			        else
+			            goTo(id);
+			        return(false);
+                 void(0);
+			    });
+			    function goTo(ancre){jQuery('html,body').animate({scrollTop:jQuery(ancre).offset().top},speed,'swing',function(){
+			          if(ancre != 'body')
+			                window.location.hash = ancre;
+			          else
+			              window.location.hash = '#';
+			          jQuery(ancre).attr('tabindex','-1');
+			          jQuery(ancre).focus();
+			          jQuery(ancre).removeAttr('tabindex');
+			      });
+			    }
+			};
+			jQuery(function(){
+				scrolling();
+			});
